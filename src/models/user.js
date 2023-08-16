bacfb62bc51dc4c133aa2d38e74cf3111c92cd06
Faraz <faraz.com>
2023-08-17 03:12:10 +0530

    SignUp logic with password encryption done

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,12 @@
 'use strict';
+
 const {
   Model
 } = require('sequelize');
+const {SALT}=require('../config/serverConfig');
+const bcrypt=require('bcrypt')
+
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -35,5 +40,10 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+  User.beforeCreate((user)=>{
+    const encryptedPassword=bcrypt.hashSync(user.password,SALT);
+    user.password=encryptedPassword;
+  });
+
   return User;
 };
