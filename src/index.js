f3f79ce32926498933ae3d5feb27eb19e3b4d03a
Faraz <faraz.com>
2023-08-18 00:08:39 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
-const express=require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const {PORT}=require('./config/serverConfig');
-const apiRoutes=require('./routes/index');
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
 
-// const {User}=require('./models/index');
-// const bcrypt=require('bcrypt');
-// const UserRepository=require('./repository/user-repository');
+// const UserService=require('./services/user-service');
+const app = express();
 
-const app=express();
+const prepareAndStartServer = () => {
 
-const prepareAndStartServer=()=>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-          app.use(bodyParser.json());
-          app.use(bodyParser.urlencoded({extended:true}));
-          
-          app.use('/api',apiRoutes);
-          
-          app.listen(PORT,async()=>{
-                    console.log(`Server Started at Port:${PORT}`);
-                    // const repo=new UserRepository();
-                    // const response=await repo.getById(1);
-                    // console.log(response);
-                    // const incommingpassword='1234546';
-                    // const user=await User.findByPk(4);
-                    // const response=bcrypt.compareSync(incommingpassword,user.password);
-                    // console.log(response);
-          })
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server Started at Port:${PORT}`);
+
+        // const service=new UserService();
+        // const newToken=service.createToken({email:'faraz@gamil.com',id:1});
+        // console.log(newToken);
+        // const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZhcmF6QGdhbWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2OTIyOTczMjksImV4cCI6MTY5MjI5NzMyOX0.YO2Fst5h3ENSTA8WgdOwZue3gm05vk1f58KkVc1d6T8';
+        // const response=service.verifyToken(token);
+        // console.log(response);
+
+    })
 }
 prepareAndStartServer();
