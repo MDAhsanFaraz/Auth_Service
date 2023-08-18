14af960f2951b6592e580cfb69c0958f9c625765
Faraz <faraz.com>
2023-08-18 20:28:37 +0530

    Added isAdmin api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -13,12 +13,16 @@ router.post(
           '/signin',
           AuthRequestValidators.validateUserAuth,
           UserController.signIn
-);
+); 
 router.get(
           '/isAuthenticated',
           UserController.isAuthenticated
 );
 
-
+router.get(
+          '/isAdmin',
+          AuthRequestValidators.validateIsAdminRequest,
+          UserController.isAdmin
+);
 
 module.exports=router;  
